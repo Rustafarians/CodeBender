ec0ab35115dc75df49d516e8815b83adc65ecf81
Maciej Górski <maciek.gorski@gmail.com>
2015-01-23 02:25:20 +0100

    Remove --force from push.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Command;
 
 fn main() {
-    Command::new("git").arg("commit").arg("-am").arg("Git commit & push on program execution.").status();
-    Command::new("git").arg("push").arg("--force").status();
+    Command::new("git").arg("commit").arg("-am").arg("Remove --force from push.").status();
+    Command::new("git").arg("push").status();
 }
