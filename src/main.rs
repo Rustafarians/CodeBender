93fd072e808ba3879ba6d7ecca722b407cfa7cc6
Maciej Górski <maciek.gorski@gmail.com>
2015-01-23 02:32:42 +0100

    Build on Travis CI.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Command;
 
 fn main() {
-    Command::new("git").arg("commit").arg("-am").arg("Remove --force from push.").status();
+    Command::new("git").arg("commit").arg("-am").arg("Build on Travis CI.").status();
     Command::new("git").arg("push").status();
 }
